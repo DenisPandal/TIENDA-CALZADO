d90a59b2f1fc2f13ff000be2b41ab11a7a06a031
DayanaLimaymanta <166547366+DayanaLimaymanta@users.noreply.github.com>
2025-10-09 22:48:42 -0500

    Update main.js
    Corrección: mantener el fondo elegido incluso al cambiar de página o cerrar el navegador.

--- tienda-calzado/main.js
+++ tienda-calzado/main.js
@@ -1,13 +1,19 @@
 const btn = document.getElementById("toggle-theme");
-const root = document.documentElement; // referencia a <html>
+const root = document.documentElement;
+
+if (localStorage.getItem("theme")==="night") {
+  root.classList.add("night");
+}
 
 btn.addEventListener("click", () => {
   root.classList.toggle("night");
 
   const img = btn.querySelector("img");
   if (root.classList.contains("night")) {
-    img.src = "imagenes/logosol.png"; 
+    img.src = "imagenes/logosol.png";
+    localStorage.setItem("theme", "night"); 
   } else {
-    img.src = "imagenes/logoluna.png"; 
+    img.src = "imagenes/logoluna.png";
+    localStorage.setItem("theme", "day"); 
   }
 });
